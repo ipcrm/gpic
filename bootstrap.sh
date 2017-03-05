8fa7870cbeb833faa059112ba33da1cf7ea177ac
ipcrm <mcadorette@gmail.com>
2017-03-05 12:48:18 -0500

    Adding Python Reqs/Exporting datasource and dash
    Adding python requirements.txt for virtualenv
    Adding Dashboard and Datasource definition exports
    Updating get_info to default missing data to unknown
    Updating boostrap.sh to have the grafana setup tasks

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,3 +10,6 @@ do
   $line
 done < /git/puppet/modules.text
 /opt/puppetlabs/bin/puppet apply --modulepath="$(/opt/puppetlabs/bin/puppet config print modulepath):/git/puppet" -e 'include gpic' --hiera_config /git/puppet/hiera.yaml
+/opt/puppetlabs/bin/puppet apply --modulepath="$(/opt/puppetlabs/bin/puppet config print modulepath):/git/puppet" -e 'include gpic' --hiera_config /git/puppet/hiera.yaml
+#curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST http://admin:admin@localhost:3000/api/datasources -d @/git/files/influxdb.datasource.json
+#curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST http://admin:admin@localhost:3000/api/dashboards/db -d @/git/files/snmp_host_dashboard.json
--- python/get_info.py
+++ python/get_info.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import paramiko
+import sys
+from influxdb import InfluxDBClient
+from influxdb import SeriesHelper
 
 # To be replaced with infile
 hosts = ['client1','client2','client3']
@@ -16,22 +19,50 @@ commands = {
 #    'diskinfo': """dmesg|grep logical\ blocks|awk '{print $5,$10,$11,$12}'|tr '\n' ','|sed 's/,$//g'""",
     'ipaddr': """ip addr show|grep inet |grep -v inet6 |grep -v 127.0.0.1|awk '{print $2}'|awk -F '/' '{print $1}'|tr '\n' ','|sed 's/,$//g'"""
 }
-
 shell_path='/bin:/usr/bin:/sbin:/usr/sbin'
 
+## InfluxDB connections settings
+host = 'localhost'
+port = 8086
+user = 'root'
+password = 'root'
+dbname = 'collectd'
+myclient = InfluxDBClient(host, port, user, password, dbname)
+
+
+class ServerInfoHelper(SeriesHelper):
+    # Meta class stores time series helper configuration.
+    class Meta:
+        # The client should be an instance of InfluxDBClient.
+        client = myclient
+        # The series name must be a string. Add dependent fields/tags in curly brackets.
+        series_name = 'snmp_data'
+        # Defines all the fields in this time series.
+        fields = ['os', 'memtotal', 'vendor', 'asset', 'bios_version', 'cputype', 'model', 'ipaddr']
+        # Defines all the tags for the series.
+        tags = ['host']
+        # autocommit must be set to True when using bulk_size
+        autocommit = True
+
 for host in hosts:
-  hostinfo = {}
-  ssh = paramiko.SSHClient()
-  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-  ssh.connect(host, username='vagrant', password='vagrant')
+  try:
+    hostinfo = {}
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, username='vagrant', password='vagrant',timeout=5)
 
-  for item, command in commands.iteritems():
-    newcommand=("export PATH=\'%s\';" % shell_path) + command
-    stdin,stdout,stderr = ssh.exec_command(newcommand)
-    hostinfo[item] = stdout.readline()[0:-1]
+    for item, command in commands.iteritems():
+      newcommand=("export PATH=\'%s\';" % shell_path) + command
+      stdin,stdout,stderr = ssh.exec_command(newcommand)
+      hostinfo[item] = stdout.readline()[0:-1]
+     
+      if item in ['vendor','asset','bios_version','cpu_type','model'] and len(hostinfo[item]) == 0:
+        hostinfo[item] = 'N/A'
 
-  print hostinfo
+    print hostinfo
 
-
+    ServerInfoHelper(host=host, **hostinfo)
+  except Exception as e:
+    print "Client [%s] - Error: [%s]" % (host,e)
 
 
