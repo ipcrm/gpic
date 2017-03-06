e95cef43c8122af3827bdca695edfa8af3a615ba
ipcrm <mcadorette@gmail.com>
2017-03-06 08:17:32 -0500

    Updating required packages

--- puppet/gpic/manifests/grafana.pp
+++ puppet/gpic/manifests/grafana.pp
@@ -1,8 +1,24 @@
 class gpic::grafana {
 
+  $gpackages = [
+    'freetype',
+    'zlib',
+    'zlib-devel',
+    'freetype-devel',
+    'freetype-demos',
+    'libfontenc',
+    'libXfont',
+    'urw-fonts',
+  ]
+
+  package {$gpackages:
+    ensure => present,
+  }
+
   class { '::grafana':
     admin_password => 'admin',
     secret_key     => 'abc123',
+    require        => Package[$gpackages],
   }
 
 }
