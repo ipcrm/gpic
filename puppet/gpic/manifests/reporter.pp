108ed14ec885ce3e6699d6b6bdb6f1bbd2e03e4e
ipcrm <mcadorette@gmail.com>
2017-03-05 21:18:20 -0500

    install fonts

--- puppet/gpic/manifests/reporter.pp
+++ puppet/gpic/manifests/reporter.pp
@@ -1,12 +1,16 @@
 class gpic::reporter {
 
-  $packages = ['golang','texlive-latex-bin','texlive-texconfig']
+  $packages = ['golang','texlive-latex-bin','texlive-texconfig','texlive-collection-fontsrecommended.noarch']
 
   package {$packages:
     ensure => present,
+    notify => Exec['rehash-texconfig'],
   }
 
-
-
+  exec {'rehash-texconfig':
+    path        => $::path,
+    command     => 'texconfig rehash',
+    refreshonly => true,
+  }
 
 }
