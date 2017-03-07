372084fba3158c102c621d3304f1ea7c994d2391
ipcrm <mcadorette@gmail.com>
2017-03-07 09:07:18 -0500

    Correcting report formatting

--- python/getimages.py
+++ python/getimages.py
@@ -10,6 +10,7 @@ from PIL import Image
 baseurl = 'http://admin:admin@localhost:3000/render/dashboard-solo/db/snmp-host-dashboard'
 
 #6 - Details
+#11 - Details Part 2
 #9 - Uptime
 #10 - Average Load
 #7 - Load Info
@@ -21,9 +22,8 @@ baseurl = 'http://admin:admin@localhost:3000/render/dashboard-solo/db/snmp-host-
 
 #Set defaults for rendering
 render = {
-    6:  {'width': 700, 'height': 307 },
-    9:  {'width': 350, 'height': 153 },
-    10: {'width': 350, 'height': 153 },
+    11:  {'width': 700, 'height': 240},
+    6:  {'width': 700, 'height': 240},
     0:  {'width': 350, 'height': 153},
 }
 
@@ -33,13 +33,14 @@ sourceHTML=open('test.html','r').read()
 for server in ['server.example.lan']:
   wrkpath = tempfile.mkdtemp()
   print wrkpath
-  for graph in [1,2,3,4,6,7,8,9,10]:
+  for graph in [1,2,3,4,6,7,8,9,10,11]:
     # Generate a tmp path
     # Set defaults if nothing custom
     if not graph in render.keys():
       render[graph] = render[0]
 
     options = "?from=now-6h&panelId=%s&theme=light&to=now&var-host=%s&width=%s&height=%s" % (graph,server,render[graph]['width'],render[graph]['height'])
+    #options = "?from=now-6h&panelId=%s&theme=dark&to=now&var-host=%s&width=%s&height=%s" % (graph,server,render[graph]['width'],render[graph]['height'])
     url = baseurl + options
 
     with open("%s/image%s.png" % (wrkpath,graph), 'wb') as f:
@@ -50,8 +51,8 @@ for server in ['server.example.lan']:
 
       im = Image.open(g)
 
-      if graph == 6:
-        imn = im.resize((500,260), Image.ANTIALIAS)
+      if graph in [6,11]:
+        imn = im.resize((500,220), Image.ANTIALIAS)
         imn.save(f, "PNG")
         imn.close()
       else:
