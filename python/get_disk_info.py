8bb601a2f6b35e0cd95ea5f67e58326fb7c93fd1
ipcrm <mcadorette@gmail.com>
2017-05-06 14:46:50 -0400

    init commit disk_space check

---
+++ python/get_disk_info.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import paramiko
+import sys
+
+# Read hosts from file
+try:
+  with open('/etc/gpic_hosts') as f:
+    hosts = f.read().splitlines()
+except Exception as e:
+    print "Errror: [%s]" % (e)
+    raise
+
+# To be replaced with infile
+commands = {
+    'mounts':"""cat /proc/mounts |egrep "ext|xfs"|awk '{print $2}'"""
+}
+
+shell_path='/bin:/usr/bin:/sbin:/usr/sbin'
+
+for host in hosts:
+  try:
+    hostinfo = {}
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, username='vagrant', password='vagrant',timeout=5)
+
+    for item, command in commands.iteritems():
+      newcommand=("export PATH=\'%s\';" % shell_path) + command
+      stdin,stdout,stderr = ssh.exec_command(newcommand)
+      hostinfo[item] = stdout.readline()[0:-1]
+
+
+    print hostinfo
+     
+  except Exception as e:
+    print "Client [%s] - Error: [%s]" % (host,e)
+
